b86ab5b550a4b5bd625aadfdd470fba47aad3f22
namansamra <namansamra1712@gmail.com>
2023-05-28 22:10:28 +0530

    main: base url change

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: process.env.BASE_URL || "http://localhost:3000/api",
+  baseURL:
+    process.env.NEXT_PUBLIC_SITE_URL ??
+    (process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000/api"),
 });
 
 api.interceptors.request.use(async (config) => {
